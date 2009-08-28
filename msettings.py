86ff8e479d3f1e8bd1e90d785a739cda7f24b0e4
Prabhu <prabhu.subramanian@gmail.com>
2009-08-29 00:26:08 +0100

    Renamed gtalk to jabberbot

--- msettings.py
+++ msettings.py
@@ -2,6 +2,9 @@ gmailUser = ''
 gmailPassword = ''
 subject = "Exchange rate for %(type)s"
 mailBody = "Exchange rate as of %(date)s is below\n\n%(rate)s\n\nService by Exchangy"
+yahooUser = ''
+yahooPassword = ''
+
 try:
     from privateSettings import *
 except:
